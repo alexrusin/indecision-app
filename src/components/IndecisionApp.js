59416476d452102c0bde0d5e2aac8485d3669c54
Alex Rusin <alex@sunriseintegration.com>
2019-04-22 09:21:01 -0700

    passing children to componets

--- src/components/IndecisionApp.js
+++ src/components/IndecisionApp.js
@@ -5,17 +5,10 @@ import Action from './Action';
 import Header from './Header';
 
 export default class IndecisionApp extends React.Component {
-    constructor(props) {
-        super(props);
-        this.handlePick = this.handlePick.bind(this);
-        this.handleDeleteOptions = this.handleDeleteOptions.bind(this);
-        this.handleAddOption = this.handleAddOption.bind(this);
-        this.handleDeleteOption = this.handleDeleteOption.bind(this);
-        
-        this.state = {
-            options: []
-        };   
-    }
+
+    state = {
+        options: []
+    }; 
 
     componentDidMount() {
         try {
@@ -42,22 +35,22 @@ export default class IndecisionApp extends React.Component {
         console.log('component will unmount');
     }
 
-    handlePick() {
+    handlePick = () => {
         const randomNum = Math.floor(Math.random() * this.state.options.length);
         alert(this.state.options[randomNum]);
-    }
+    };
 
-    handleDeleteOptions() {
+    handleDeleteOptions = () => {
         this.setState(() => ({ options: []}));
-    }
+    };
 
-    handleDeleteOption(optionToRemove) {
+    handleDeleteOption = (optionToRemove) => {
         this.setState((prevState) => ({
             options: prevState.options.filter((option) => optionToRemove !== option)
         }));
-    }
+    };
 
-    handleAddOption(option) {
+    handleAddOption = (option) => {
         if (!option) {
             return 'Enter valid value to add item';
         } else if (this.state.options.indexOf(option) > -1) {
@@ -65,7 +58,7 @@ export default class IndecisionApp extends React.Component {
         } else {
             this.setState((prevState) => ({options: prevState.options.concat(option)}));
         }
-    }
+    };
 
     render() {
         const subtitle = 'Put your life in the hands of a computer';
